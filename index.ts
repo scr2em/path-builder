92962da92748ee8acb76b57d69364d44921a614c
Mohamed Abdelgwad <4671486+scr2em@users.noreply.github.com>
2024-07-22 16:27:34 +0300

    fix publish config

--- index.ts
+++ index.ts
@@ -1,17 +1,10 @@
 /**
- * Transforms the properties of an object `obj` such that accessing any property returns a path string.
- * The path is constructed based on the `basePath` and the keys of the object leading to that property.
- * This function recursively processes each property that is an object itself (excluding arrays and null values),
- * appending each key to the current path. For leaf properties, it replaces the property with a getter that
- * returns the full path to the property value prefixed by `basePath`.
- *
  * @template T The shape/type of the object being passed.
  * @param {T} obj - The object whose properties will be transformed into path-returning getters.
  * @param {string} basePath - The base path to be prefixed to all generated paths.
  * @returns {T} The modified object where each property returns a path string when accessed.
  *
  * @example
- * // Assuming you have an object like this:
  * const myObj = { user: { id: '123', profile: { name: 'John' } } };
  * // and you use PathBuilder with a basePath of '/api'
  * const apiPaths = PathBuilder(myObj, '/api');
